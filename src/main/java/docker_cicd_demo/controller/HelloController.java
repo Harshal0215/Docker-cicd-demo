5fb46486b5c6c103fd7e199f95e9c1a10df7b782
Harshal <151913213+Harshal0215@users.noreply.github.com>
2026-08-13 12:07:26 +0530

    Fix Docker Hub authentication

--- src/main/java/docker_cicd_demo/controller/HelloController.java
+++ src/main/java/docker_cicd_demo/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/health")
     public String health() {
-        return "Application is healthy.";
+        return "Application is healthy now.";
     }
 }
